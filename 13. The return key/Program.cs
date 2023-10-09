c537a58ecdd0005f9ca33ab2975a13377b721df3
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-09 21:16:23 +0200

    Add project files.

---
+++ 13. The return key/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.The_return_key
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cube(5));
+            Cube(5);
+            Console.ReadLine();
+        }
+        static int Cube(int number)
+        {
+
+            int result = number * number * number;
+            return result;
+        }
+    }
+}      
